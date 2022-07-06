38de67fb67a5d3d93ea09ae3d13944dc15fbd6f4
subservers <anubis@iname.com>
2022-07-07 05:48:30 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import datetime
+import requests
+import hashlib
+import hmac
+import json
+
+
+def format_body(data: dict) -> str:
+    return json.dumps(data, separators=(',', ':'))
+
+
+def gen_ts() -> str:
+    return f"{datetime.datetime.now():%Y%m%d%H%M%S%f}"[:-3]
+
+
+def gen_xsk(data: str) -> str:
+    data += "@#?.#@"
+    secret = "MaYoaMQ3zpWJFWtN9mqJqKpHrkdFwLd9DDlFWk2NnVR1mChVRI6THVe6KsCnhpoR"
+    return hmac.new(secret.encode(), msg=data.encode(), digestmod=hashlib.sha256).hexdigest()
+
+
+def get_arena_data(zone_name: str, server_name: str, role_id: str, device_id: str, token: str) -> dict:
+    param = {
+        "role_id": role_id,
+        "zone": zone_name,
+        "server": server_name,
+        "ts": gen_ts()
+    }
+    param = format_body(param)
+    headers = {
+        'Host': 'm.pvp.xoyo.com',
+        'accept': 'application/json',
+        'deviceid': device_id,
+        'platform': 'ios',
+        'gamename': 'jx3',
+        'clientkey': '1',
+        'cache-control': 'no-cache',
+        'apiversion': '1',
+        'sign': 'true',
+        'token': token,
+        'Content-Type': 'application/json',
+        'Connection': 'Keep-Alive',
+        'User-Agent': 'SeasunGame/178 CFNetwork/1240.0.2 Darwin/20.5.0',
+        "X-Sk": gen_xsk(param)
+    }
+    data = requests.post(url="https://m.pvp.xoyo.com/role/indicator", data=param, headers=headers)
+    return data.json()
+
+
+if __name__ == '__main__':
+    result = get_arena_data(
+        zone_name="",
+        server_name="",
+        role_id="",
+        device_id="",
+        token=""
+    )
+    print(result)
